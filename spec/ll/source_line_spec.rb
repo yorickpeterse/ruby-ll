84e12b619ff1214308e4f253171c0655352a0c1d
Yorick Peterse <yorickpeterse@gmail.com>
2015-01-09 00:27:01 +0100

    Specs for SourceLine#initialize.

--- spec/ll/source_line_spec.rb
+++ spec/ll/source_line_spec.rb
@@ -1,6 +1,28 @@
 require 'spec_helper'
 
 describe LL::SourceLine do
+  describe '#initialize' do
+    before do
+      @source_line = described_class.new('foo')
+    end
+
+    it 'sets the data' do
+      @source_line.data.should == 'foo'
+    end
+
+    it 'sets the line number' do
+      @source_line.line.should == 1
+    end
+
+    it 'sets the column' do
+      @source_line.column.should == 1
+    end
+
+    it 'sets the file name' do
+      @source_line.file.should == '(ruby)'
+    end
+  end
+
   describe '#source' do
     it 'returns the source code of the line' do
       line = described_class.new(<<-EOF.strip, 2, 3)
