244bbdae85ad70619b234c2053e69f24be1f4c36
Yorick Peterse <yorickpeterse@gmail.com>
2015-02-05 01:14:41 +0100

    Basic code generator setup.
    This doesn't have any tests yet but it's a starting point.

--- lib/ll/code_generator.rb
+++ lib/ll/code_generator.rb
@@ -1,15 +1,27 @@
 module LL
   ##
-  # The CodeGenerator class takes a {LL::CompiledGrammar} instance and turns it
-  # into a block of Ruby source code that can be used as an actual LL(1) parser.
+  # The CodeGenerator class takes a {LL::CompiledConfiguration} instance and
+  # turns it into a block of Ruby source code that can be used as an actual
+  # LL(1) parser.
   #
   class CodeGenerator
     ##
-    # @param [LL::CompiledGrammar] compiled
+    # The ERB template to use for code generation.
+    #
     # @return [String]
     #
-    def generate(compiled)
-      # TODO
+    TEMPLATE = File.expand_path('../driver_template.erb', __FILE__)
+
+    ##
+    # @param [LL::CompiledConfiguration] config
+    # @return [String]
+    #
+    def generate(config)
+      context  = ERBContext.new(:config => config)
+      template = File.read(TEMPLATE)
+      erb      = ERB.new(template, nil, '-').result(context.get_binding)
+
+      return erb
     end
   end # CodeGenerator
 end # LL
