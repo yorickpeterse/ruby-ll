51de759e3d948a3fb4f530832157b39939c4b43a
Yorick Peterse <yorickpeterse@gmail.com>
2015-02-09 20:46:48 +0100

    Basic YARD docs for the DriverConfig class.

--- lib/ll/driver_config.rb
+++ lib/ll/driver_config.rb
@@ -5,18 +5,30 @@ module LL
   class DriverConfig
     attr_reader :terminals, :rules, :table, :actions
 
+    ##
+    # @param [Array] array
+    #
     def terminals=(array)
       self.terminals_native = @terminals = array
     end
 
+    ##
+    # @param [Array] array
+    #
     def rules=(array)
       self.rules_native = @rules = array
     end
 
+    ##
+    # @param [Array] array
+    #
     def table=(array)
       self.table_native = @table = array
     end
 
+    ##
+    # @param [Array] array
+    #
     def actions=(array)
       self.actions_native = @actions = array
     end
