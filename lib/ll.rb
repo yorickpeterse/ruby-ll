4202b630ce3517fb23e895199e447b92deb0a319
Yorick Peterse <yorickpeterse@gmail.com>
2015-02-13 00:34:04 +0100

    Fixed require setup for generated parsers.

--- lib/ll.rb
+++ lib/ll.rb
@@ -6,23 +6,11 @@ require 'optparse'
 require 'ast'
 require 'ansi/code'
 
-require_relative 'll/version'
-require_relative 'll/driver'
-require_relative 'll/driver_config'
-
-require_relative 'libll'
-
-#:nocov:
-if RUBY_PLATFORM == 'java'
-  org.libll.Libll.load(JRuby.runtime)
-end
-#:nocov:
-
+require_relative 'll/setup'
 require_relative 'll/source_line'
 require_relative 'll/lexer'
 require_relative 'll/token'
 require_relative 'll/parser'
-require_relative 'll/parser_error'
 require_relative 'll/grammar_compiler'
 require_relative 'll/code_generator'
 require_relative 'll/compiled_grammar'
---
+++ lib/ll/setup.rb
@@ -0,0 +1,11 @@
+require_relative 'version'
+require_relative 'driver'
+require_relative 'driver_config'
+require_relative 'parser_error'
+require_relative '../libll'
+
+#:nocov:
+if RUBY_PLATFORM == 'java'
+  org.libll.Libll.load(JRuby.runtime)
+end
+#:nocov:
