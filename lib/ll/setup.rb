c8bda3305eafc0e87734ac2acf30e1947f4ce0a8
Yorick Peterse <yorickpeterse@gmail.com>
2015-02-16 00:44:27 +0100

    Require ConfigurationCompiler in ll/setup

--- lib/ll/setup.rb
+++ lib/ll/setup.rb
@@ -2,6 +2,7 @@ require_relative 'version'
 require_relative 'driver'
 require_relative 'driver_config'
 require_relative 'parser_error'
+require_relative 'configuration_compiler'
 require_relative '../libll'
 
 #:nocov:
